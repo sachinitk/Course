4504967a5c8fa8318cd285951511fb6820cec73b
Sachin Yadav <sachinitk@bitbucket.org>
2019-05-24 16:57:27 +0530

    Find avg age

---
+++ src/Course.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Course {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the no of courses");
+        int nc = scanner.nextInt();
+        if(nc <= 0){
+            System.out.println("Invalid range");
+        }
+        String[] c = new  String[nc];
+        System.out.println("Enter the courses");
+        for(int i = 0 ; i < nc; i++){
+            c[i] = scanner.nextLine();
+        }
+        System.out.println("Enter the course to be searched");
+        String search = scanner.nextLine();
+        for(int i = 0 ; i < nc ; i++){
+            if(search.equals(c[i])){
+                System.out.println(c[i]+"is the available courses");
+                return;
+            }
+        }
+        System.out.println("Course is not available");
+
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter total no. of employees:");
+        int n = scanner.nextInt();
+        if(n<2){
+            System.out.println("Please enter a valid employee count");
+            return;
+        }
+        int array[] = new int[n];
+        for(int i = 0 ; i < n ; i++){
+
+        }
+    }
+}
