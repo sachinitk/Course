cf316cd67373f90d84c2cf2d53a8b6bdaed680f6
Sachin Yadav <sachinitk@bitbucket.org>
2019-06-06 18:21:29 +0530

    replace word done
    Signed-off-by: Sachin Yadav <sachinitk@bitbucket.org>

---
+++ src/array_strings/ReplaceWord.java
@@ -0,0 +1,27 @@
+package array_strings;
+
+import java.util.Scanner;
+
+public class ReplaceWord {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = "";
+        System.out.println("Enter the string:");
+        str += scanner.nextLine();
+        String[] sentence  = str.split(" ");
+        System.out.println("Enter the word to be searched:");
+        String wordc = scanner.nextLine();
+        int flag =0;
+        System.out.println("Enter the word to be replaced:");
+        String wordr = scanner.nextLine();
+        for(String word : sentence){
+            if(word.equals(wordc))
+                flag =1;
+        }
+        if(flag == 0) {
+            System.out.println("The word " + wordc + " not found");
+            return;
+        }
+        System.out.println(str.replaceAll(wordc,wordr));
+    }
+}
